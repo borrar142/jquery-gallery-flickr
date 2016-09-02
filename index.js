011a6b86edd6360dab7d2e1609817709a554a843
Jesús Olazagoitia <jesus.designer@gmail.com>
2016-09-02 21:47:30 +0200

    integrado con express

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
+var express = require('express');
+var app = express();
 
-function markeclaudioFlickrBox(id){
-$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id="+id+"&lang=it-it&format=json&jsoncallback=?", function(data){
-        $.each(data.items, function(i,item){
-            $("<img class='imgflikr' />")
-            .attr("src", item.media.m).appendTo("#app")
-            .wrap("<div class='imagesflickr'><a title='"+item.tags+"' rel='prettyPhoto[flickrgallery]' href='"+item.media.m.replace("_m.jpg", "_z.jpg")+"'></a></div>")
-            .mouseover(function() {
-              $(this).css("filter","alpha(opacity=70)");
-              $(this).css("-moz-opacity",".70");
-              $(this).css("opacity",".70");
-              
-            }).mouseout(function(){
-               $(this).css("filter","alpha(opacity=1)");
-              $(this).css("-moz-opacity","1");
-              $(this).css("opacity","1");
-              
-            });
-            
-            });
-       
-    });
-    
-} 
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html'); 
+  //res.send(index);
+});
+
+app.listen(3000, function () {
+  console.log('corriendo en el puerto 3000');
+});
